6a4b8816685e614bfdeabe8ad2826e425ba3943e
tayutaedomo <tayutaedomo@gmail.com>
2017-11-16 02:04:11 +0900

    Add puppeteer/capture_viewport

--- routes/puppeteer.js
+++ routes/puppeteer.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var validUrl = require('valid-url');
 var puppeteer = require('puppeteer');
+var _ = require('underscore');
 
 var router = express.Router();
 
@@ -27,6 +28,44 @@ router.post('/capture', function(req, res, next) {
 
       const page = await browser.newPage();
       await page.goto(urlToScreenshot);
+      await page.screenshot().then(function(buffer) {
+        res.setHeader('Content-Disposition', 'attachment;filename="' + urlToScreenshot + '.png"');
+        res.setHeader('Content-Type', 'image/png');
+        res.send(buffer)
+      });
+
+      await browser.close();
+    })();
+
+  } else {
+    res.send('Invalid url: ' + urlToScreenshot);
+  }
+});
+
+router.post('/capture_viewport', function(req, res, next) {
+  var urlToScreenshot = parseUrl(req.body.url);
+
+  if (validUrl.isWebUri(urlToScreenshot)) {
+    console.log('Screenshotting: ' + urlToScreenshot);
+    (async() => {
+      const browser = await puppeteer.launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+      });
+
+      const page = await browser.newPage();
+
+      await page.goto(urlToScreenshot);
+
+      let viewport = {
+        width: parseInt(req.body.width),
+        height: parseInt(req.body.height),
+        deviceScaleFactor: 1,
+        isMobile: false,
+        hasTouch: false,
+        isLandscape: false
+      };
+      await page.setViewport(viewport);
+
       await page.screenshot().then(function(buffer) {
         res.setHeader('Content-Disposition', 'attachment;filename="' + urlToScreenshot + '.png"');
         res.setHeader('Content-Type', 'image/png');
